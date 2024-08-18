5ff038faf2d036cde365c74dbc5f0dca6e10b0bb
Caleb Joseph <175043934+Caleb-A-J@users.noreply.github.com>
2024-08-18 11:10:20 -0700

    Completed weekend LeetCode/Homework.

---
+++ LeetCode/ValidAnagram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+
+            }
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach
+                    (char c in s)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+
+                }
+            }
+
+            foreach (char c in t)
+            {
+                if (!charCount.ContainsKey(c) || charCount[c] == 0)
+                {
+                    return false;
+                }
+                charCount[c]--;
+            }
+
+            return true;
+        }
+    }
+}
